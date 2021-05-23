96cab9610c525ca8c6feb4ff315c14c6d465f250
heoquaycutekhung <20120442@student.hcmus.edu.vn>
2021-05-23 21:48:36 +0700

    upload file

--- DAMH/DAMH/Source1.cpp
+++ DAMH/DAMH/Source1.cpp
@@ -3,19 +3,24 @@
 #include <cmath>
 using namespace std;
 //
+//struct ngày tháng năm(ngày sinh)
 struct Date {
 	int Date;
 	int Month;
 	int Year;
 };
+//struct thông tin học sinh hoặc giáo viên, nếu là giáo viên thì 1 số thông tin bỏ trống
 struct Data {
 	int IDstd;
 	char* name;
 	int sex;// 0 là nam, 1 là nữ.
 	Date Bir;// ngày sinh
 	int IDsocial;
+	char* Class;
+	MarkNode* phead;// nhiều môn
 };
 //
+//struct thời gian( giời, phút, ngày, tháng)-> lịch học của sinh viên
 struct schedule {// thời gian học
 	int Date;
 	int Month;
@@ -23,11 +28,16 @@ struct schedule {// thời gian học
 	int minute;
 };
 //
+// struct điểm 1 môn của 1 sinh viên
 struct MarkNode {
 	int ID;// linh hoạt trong điểm của sinh viên thì là id môn học, còn trong môn học thì là id sinh viên
-	int Mark;
+	float Total_Mark;
+	float Final_Mark;
+	float Midterm_Mark;
+	float Other_Mark;
 	MarkNode* pNext;
 };
+// struct 1 khóa học, 1 số thông tin có thể trống
 struct Course{
 	int ID_course;
 	char* name;
@@ -36,12 +46,6 @@ struct Course{
 	int Num_of_std_now;
 	int Max_student;
 	schedule Schedule;// thời khóa biểu môn học
-	MarkNode* pHead;
-};
-//
-struct Mark {
-	int IDstd;
-	int Num_of_course;
 	MarkNode* pHead;
 };
 //
