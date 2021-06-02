7d3f4337c61e6c62fe4c2eddb79294b02bcba33a
nthieu2212 <83766319+nthieu2212@users.noreply.github.com>
2021-06-02 20:49:28 +0700

    read course

--- DAMH/DAMH/read_data.h
+++ DAMH/DAMH/read_data.h
@@ -5,5 +5,6 @@ const string txt_tail = ".txt";
 void read_1_info(User& A, ifstream &f);
 void read_info(User& A);
 void Output_info(User A);
-
+void read_course(User& A,MarkNode* &view);
+void read1CourseInfor(Course& A, ifstream& f);
 #endif
