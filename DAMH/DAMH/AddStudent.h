89903c563f6182cd368823107d853139ab2f48f6
that161 <tranvanthat9a1@gmail.com>
2021-06-29 08:06:28 +0700

    Create AddStudent.h

---
+++ DAMH/DAMH/AddStudent.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "change_info.h"
+#include "ConsoleProcess.h"
+#include "Course_function.h"
+#include "Header.h"
+#include "ListClass.h"
+#include "listCourse.h"
+#include "login.h"
+#include "Menu.h"
+
+void AddStudentToClass();
+void AddRandomInfoStudentToClass();
+void MenuAddInfoStudentToClass();
+int RunMenuAddInfoStudentToClass();
